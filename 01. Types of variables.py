64747de373a4f160e1ed64b969098875b96c21d6
Marta <42750541+martaksx@users.noreply.github.com>
2018-08-27 21:58:32 +0200

    Update 01. Types of variables.py

--- 01. Types of variables.py
+++ 01. Types of variables.py
@@ -1,110 +1,25 @@
-
-# coding: utf-8
-
-# In[1]:
-
-
 #To run the line press shift+enter
 #integer
 x=2
-
-
-# In[2]:
-
-
 x
-
-
-# In[3]:
-
-
 type(x)
-
-
-# In[4]:
-
-
 #how to add a delimeter? Change to markdown, put --- and press shift+enter
-
-
 # ---
-
-# In[5]:
-
-
 #float/double
 y=2.5
-
-
-# In[6]:
-
-
 y
-
-
-# In[7]:
-
-
 type(y)
-
-
-# In[8]:
-
-
 #next tip: if you want to move out from the edit mode (green on left), just press escape, then it turns blue and you can move up and down with arrows
-
-
-# In[9]:
-
-
 #Also next tip: when you are not in editing mode, click M on keyoboard, then it changes to Markdown and you can add the delimeter
-
-
 # ---
-
-# In[15]:
-
-
 #string
 a="hello"
 b='2'
-
-
-# In[11]:
-
-
 a
-
-
-# In[12]:
-
-
 type(a)
-
-
-# In[16]:
-
-
 b
-
-
 # ---
-
-# In[17]:
-
-
 #logical/boolean
 q1=True
-
-
-# In[18]:
-
-
 q1
-
-
-# In[19]:
-
-
 type(q1)
-
